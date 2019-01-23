9de7b4d673a9fd1114c57934328313543e12a177
lyf0327 <yafeng.li@samsung.com>
2019-01-23 21:36:03 +0800

    add pv pvc in data-common

--- personal-files/python-test/test.py
+++ personal-files/python-test/test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding:UTF-8 -*-
-import time
-print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-time.sleep(2)
-print 'hh'
+for n in range(100,999):
+    x = n / 100
+    y = n / 10 % 10
+    z = n % 10
+    if n == x ** 3 +y ** 3 +z ** 3:
+        print n
