80ec02d4d7d6f779f782713e4bf873132f642e44
lyf0327 <yafeng.li@samsung.com>
2019-02-22 15:29:23 +0800

    add

--- personal-files/shell/osd-disk.sh
+++ personal-files/shell/osd-disk.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+help(){
+        echo "Usage: osd-disk.sh <num1> <num2> <num3> ..."
+}
+if [[ "$1" == "" ]]
+then
+        help
+fi
 for i in $*
 do
     LV_ID=$(cat /var/lib/ceph/osd/ceph-$i/fsid)
