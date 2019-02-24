aa6b4066e7838896425494480f2774b249099022
lyf0327 <yafeng.li@samsung.com>
2019-02-24 16:39:26 +0800

    add gpu monintoring

--- personal-files/shell/docker_image_download.sh
+++ personal-files/shell/docker_image_download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-image = $1
-tag = $2
+image=$1
+tag=$2
 help(){
     echo "Usage: docker_image_download.sh  <image> <tag>"
 }
@@ -10,9 +10,9 @@ then
 else
     mkdir -p /lyf/images && cd /lyf/images
     ssh -i /sshlogin/FARM-1471-5b25a9a4.cn-north-1.pem 52.80.148.5 "docker pull ${image}:${tag}\
-     &&mkdir /lyf &&docker save ${image}:${tag} -o /lyf/${image}.tar"
-    scp -i /sshlogin/FARM-1471-5b25a9a4.cn-north-1.pem 52.80.148.5:/lyf/${image}.tar ./
-    docker load -i ${image}.tar
+     &&mkdir -p /lyf &&docker save ${image}:${tag} -o /lyf/${image//\//-}.tar"
+    scp -i /sshlogin/FARM-1471-5b25a9a4.cn-north-1.pem 52.80.148.5:/lyf/${image//\//-}.tar ./
+    docker load -i ${image//\//-}.tar
     docker tag ${image}:${tag} registry.gcloud.srcb.com/${image}:${tag}
     docker push registry.gcloud.srcb.com/${image}:${tag}
     if [ $? -ne 0 ]
